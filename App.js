92925c9bde0fc07105b291952a2eca835ba151cd
Osama-14 <mohdosama42000@gmail.com>
2022-03-29 16:48:04 +0500

    feels/windspeed/pressure/humidity

--- App.js
+++ App.js
@@ -1,120 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import * as Location from 'expo-location'
-import { useEffect,useState } from 'react';
-import WeatherInfo from './components/WeatherInfo';
-import UnitsPicker from './components/UnitsPicker';
-import { colors } from './utils/index';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import WeatherInfo from "./components/WeatherInfo";
+import UnitsPicker from "./components/UnitsPicker";
+import { colors } from "./utils/index";
+import ReloadIcon from "./components/ReloadIcon";
+import WeatherDetails from "./components/WeatherDetails";
 
-const WEATHER_API_KEY = 'dba6a2f6187b02f0a210de34fb78db54'
-const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
-
-
+const WEATHER_API_KEY = "dba6a2f6187b02f0a210de34fb78db54";
+const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState("metric");
 
-  const [errorMessage, setErrorMessage]= useState(null)
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [unitsSystem, setUnitsSystem] = useState('metric')
+  useEffect(() => {
+    load()
+  }, [unitsSystem]);
 
+  const load = async () => {
+    setCurrentWeather(null);
+    setErrorMessage(null);
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
 
+      console.log(status);
 
-useEffect(() => {
-  load()
-}, [unitsSystem])
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app");
+        return;
+      }
 
-const load = async () => {
-  setCurrentWeather(null)
-  setErrorMessage(null)
-  try{
-    let {status} = await Location.requestForegroundPermissionsAsync()
-  
-    console.log(status)
+      const location = await Location.getCurrentPositionAsync();
 
-    if (status !== 'granted') {
-      setErrorMessage('Access to location is needed to run the app')
-      return
+      const { latitude, longitude } = location.coords;
+
+      console.log("latitude, longitude", latitude, longitude);
+
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
+
+      const response = await fetch(weatherUrl);
+
+      const result = await response.json();
+
+      console.log(response);
+      if (response.ok) {
+        setCurrentWeather(result);
+      } else {
+        console.log("-----", result.message);
+        setErrorMessage(result.message);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
     }
+  };
 
-    const location = await Location.getCurrentPositionAsync()
-    
-    const {latitude, longitude} = location.coords
-
-    console.log("latitude, longitude", latitude, longitude)
-
-    const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
-
-    const response = await fetch(weatherUrl)
-
-    const result = await response.json()
-
-    console.log(response)
-    if(response.ok) {
-      setCurrentWeather(result)
-    }else{
-      console.log("-----", result.message)
-      setErrorMessage(result.message)
-    }
-
-   
-   
-
-  } catch (error) {
-    setErrorMessage(error.message)
-  }
-
-
-}
-
-if(currentWeather) {
-
- return (
-    <View style={styles.container}>
-            <StatusBar style="auto" />
-      <View style={styles.main}>
-
-        <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem}/>
-
-      <WeatherInfo currentWeather={currentWeather} />
-
+  if (currentWeather) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <UnitsPicker
+            unitsSystem={unitsSystem}
+            setUnitsSystem={setUnitsSystem}
+          />
+          <ReloadIcon load={load} />
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
+        <WeatherDetails
+          currentWeather={currentWeather}
+          unitsSystem={unitsSystem}
+        />
       </View>
-
-      
-    </View>
-  )
-}else if(errorMessage){
-  return(
-    <View style={styles.container}>
-    <Text>{errorMessage}</Text>
-    <StatusBar style="auto" />
-  </View>
-
-  )
-} else {
-  return(
-    <View style={styles.container}>
-    <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
-    <StatusBar style="auto" />
-  </View>
-
-  )
+    );
+  } else if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
-}
-
-
-
-
-
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   main: {
-    justifyContent: 'center',
+    justifyContent: "center",
     flex: 1,
-  }
+  },
 });
--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,19 +1,60 @@
-import { View, Text, StyleSheet, Platform } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { Picker } from "@react-native-community/picker";
 
 export default function UnitsPicker({ unitsSystem, setUnitsSystem }) {
+  const [label, setLabel] = useState("C°");
+  const [isLabel, showLabel] = useState(false);
   return (
     <View style={styles.unitsSystem}>
-      <Picker
+      {/* <Picker
         selectedValue={unitsSystem}
         onValueChange={(item) => setUnitsSystem(item)}
         mode="dropdown"
         itemStyle={{ fontSize: 12 }}
+      
       >
         <Picker.Item label="C°" value="metric" />
         <Picker.Item label="F°" value="imperial" />
-      </Picker>
+      </Picker> */}
+
+      {!isLabel ? (
+        <TouchableOpacity
+          style={{ margin: 5, marginLeft: 10 }}
+          onPress={() => showLabel(true)}
+        >
+          <Text style={{ fontSize: 12 }}>{label}</Text>
+        </TouchableOpacity>
+      ) : (
+        <View style={{ margin: 5, width:"100%" }}>
+          <TouchableOpacity
+            style={{ padding: 10, width: "100%" }}
+            onPress={() => {
+              setLabel("C°");
+              showLabel(false);
+              setUnitsSystem("metric");
+            }}
+          >
+            <Text>C°</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ padding: 10, width: "100%" }}
+            onPress={() => {
+              setLabel("F°");
+              showLabel(false);
+              setUnitsSystem("imperial");
+            }}
+          >
+            <Text>F°</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -26,7 +67,7 @@ const styles = StyleSheet.create({
         top: -20,
       },
       android: {
-        top: 20,
+        top: 40,
       },
     }),
     height: 50,
